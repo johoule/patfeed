ca2c591b5f6d1512983c454c5f95ba352aabdb0d
Jon Cooper <joncoop@users.noreply.github.com>
2017-04-18 19:00:18 -0400

    Add files via upload

---
+++ js/checker.js
@@ -0,0 +1,62 @@
+function checkIt() {
+
+  // Initialize scores
+  var nerd = 0;
+  var jock = 0;
+  var cheer = 0;
+  var hipster = 0;
+  var band = 0;
+  var emo = 0;
+
+  // Get all checkbox elements from form
+  var elements = document.getElementsByClassName('answer');
+
+  // Tally results
+  for (var e of elements) {
+
+    if (e.checked) {
+      if (e.value == 'nerd') {
+        nerd++;
+      }
+      else if (e.value == 'jock') {
+        jock++;
+      }
+      else if (e.value == 'cheer') {
+        cheer++;
+      }
+    }
+
+  }
+
+  // Determine result
+  var counts = "Nerd: " + nerd + ", " +
+               "Jock: " + jock  + ", " +
+               "Cheer: " + cheer;
+
+  // What is the highest value?
+  var max = Math.max(nerd, jock, cheer);
+
+  // Form a message
+  var message;
+  var picture;
+
+  if (max == nerd) {
+    message = "Looks like you took a break from playing video games and coding websites, you nerd.";
+    picture = "<img src='img/nerd.jpg'>";
+  }
+  else if (max == jock) {
+    message = "Do you even lift, bro? Of course you do. You're clearly a jock.";
+  }
+  else if (max == cheer) {
+    message = "Hip hip, hooray! You are such a cheerleader. Go sports!";
+  }
+  else {
+    message = "Uh, oh! Something went wrong.";
+  }
+
+  // Display result
+  document.getElementById('result').style.display = "block";
+  document.getElementById('result').innerHTML = counts;
+  // document.getElementById('pic').innerHTML = picture;
+  // document.getElementById('result').innerHTML = message;
+}
